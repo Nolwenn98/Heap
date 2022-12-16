9b811e687e325c02f56eb0b8f1646aee4b0308d3
nolwenn david <nolwenn-david@hotmail.fr>
2022-12-16 10:16:51 +0100

    add test

--- heap_linked_list/test_heap_ll.c
+++ heap_linked_list/test_heap_ll.c
@@ -5,109 +5,118 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
-#include "list_functions.h"
+#include "list.h"
 #include "heap_ll.h"
+
+static list_t libre = LIST_INITIALIZER;
 
 void test_init_heap(void)
 {
-    init_heap();
+    init_heap(&libre);
 
     CU_ASSERT(heap[0] == 127);
     CU_ASSERT(heap[1] == FREE_ZONE);
-    CU_ASSERT(libre == 0);
+    CU_ASSERT((int)libre.start->data == 0);
+    CU_ASSERT((int)libre.end->data == 0);
+
+    // Remise à zero de libre
+    list_append(&libre, (void *)4);
+    init_heap(&libre);
+
+    CU_ASSERT((int)libre.start->data == 0);
+    CU_ASSERT((int)libre.end->data == 0);
 }
 
 void test_first_fit(void)
 {
     int size = 3;
+    int data1 = 4;
 
-    init_heap();
+    // Toute la mémoire libre
+    init_heap(&libre);
 
-    CU_ASSERT(first_fit(size) == 0);
+    CU_ASSERT(first_fit(size, &libre) == 0);
 
+    // Une zone mémoire alloué
     heap[0] = 3;
     strcpy(&heap[1], "ab");
+    list_pop_first(&libre);
     heap[4] = 123;
     heap[5] = -1;
-    libre = 4;
+    list_append(&libre, (void *)data1);
 
-    CU_ASSERT(first_fit(size) == 4);
+    CU_ASSERT(first_fit(size, &libre) == 4);
 
-    heap[0] = 3;
-    strcpy(&heap[1], "ab");
-    heap[4] = 1;
-    heap[5] = -1;
-    heap[6] = 3;
-    strcpy(&heap[7], "ab");
-    heap[10] = 118;
-    heap[11] = -1;
-    libre = 4;
-
-    CU_ASSERT(first_fit(size) == 10);
+    // Toute la mémoire alloué
+    init_heap(&libre);
 
     heap[0] = 127;
     heap[127] = '\0';
-    libre = -1;
+    list_pop_first(&libre);
 
-    CU_ASSERT(first_fit(size) == FREE_ZONE);
+    CU_ASSERT(first_fit(size, &libre) == MEMORY_FULL);
 }
 
 void test_add_character(void)
 {
-    char *p1;
+    char *p1, *p2, *p3;
+    strategie = &first_fit;
 
-    init_heap();
-    p1 = (char *)heap_malloc(3);
+    // Toute la mémoire libre
+    init_heap(&libre);
+    p1 = (char *)heap_malloc(3, &libre, strategie);
     strcpy(p1, "ab");
 
     CU_ASSERT(heap[0] == 3 && heap[1] == 'a' && heap[2] == 'b' && heap[3] == FREE_BLOCK)
     CU_ASSERT(heap[4] == 123 && heap[5] == FREE_ZONE);
-    CU_ASSERT(libre == 4);
-}
+    CU_ASSERT((int)libre.start->data == 4);
 
-void test_empty_heap(void)
-{
-    char *p1;
+    // Une zone mémoire alloué
+    p2 = (char *)heap_malloc(123, &libre, strategie);
+    strcpy(p2, "aaaaab");
 
-    init_heap();
-    p1 = (char *)heap_malloc(127);
-    strcpy(p1, "ab");
+    CU_ASSERT(heap[4] == 123 && heap[5] == 'a')
+    CU_ASSERT((int)libre.start == NULL);
 
-    CU_ASSERT(heap[0] == 127 && heap[127] == FREE_BLOCK);
-    CU_ASSERT(libre == FREE_ZONE);
+    // Toute la mémoire alloué
+    p3 = (char *)heap_malloc(4, &libre, strategie);
+
+    CU_ASSERT(p3 == NULL)
+    CU_ASSERT((int)libre.start == NULL);
 }
 
 void test_add_to_empty_heap(void)
 {
     char *p1, *p2;
 
-    init_heap();
-    p1 = (char *)heap_malloc(127);
+    init_heap(&libre);
+    p1 = (char *)heap_malloc(127, &libre, strategie);
     strcpy(p1, "ab");
 
-    p2 = (char *)heap_malloc(3);
+    p2 = (char *)heap_malloc(3, &libre, strategie);
 
     CU_ASSERT(p2 == NULL);
-    CU_ASSERT(libre == FREE_ZONE);
+    CU_ASSERT(libre.start == NULL);
+    CU_ASSERT(libre.end == NULL);
 }
 
 void test_add_to_almost_empty_heap(void)
 {
     char *p1, *p2;
 
-    init_heap();
-    p1 = (char *)heap_malloc(125);
+    init_heap(&libre);
+    p1 = (char *)heap_malloc(125, &libre, strategie);
     strcpy(p1, "ab");
 
-    p2 = (char *)heap_malloc(3);
+    p2 = (char *)heap_malloc(3, &libre, strategie);
 
     CU_ASSERT(p2 == NULL);
-    CU_ASSERT(libre == 126);
+    CU_ASSERT((int)libre.start->data == 126);
 }
 
 void test_heap_free(void)
 {
-    init_heap();
+    init_heap(&libre);
 
     heap[0] = 3;
     strcpy(&heap[1], "ab");
@@ -122,105 +131,7 @@ void test_heap_free(void)
     heap_free(&heap[5]);
 
     CU_ASSERT(heap[4] == 2 && heap[5] == FREE_ZONE);
-    CU_ASSERT(libre == 4);
-}
-
-void test_heap_malloc_example()
-{
-    init_heap();
-
-    char *p1 = heap_malloc(10);
-
-    CU_ASSERT(p1 - 1 == heap);
-    CU_ASSERT(*(p1 + 1) != FREE_ZONE);
-
-    CU_ASSERT(libre == 10 + 1);
-    CU_ASSERT(*(heap + libre) == 116);
-    CU_ASSERT(*(heap + libre + 1) == FREE_ZONE);
-
-    char *p2 = heap_malloc(9);
-
-    CU_ASSERT(p2 == heap + 12);
-    CU_ASSERT(*(p2 - 1) == 9);
-    CU_ASSERT(libre == 21);
-
-    char *p3 = heap_malloc(5);
-
-    CU_ASSERT(p3 == heap + 22);
-    CU_ASSERT(*(p3 - 1) == 5);
-    CU_ASSERT(libre == 27);
-
-    char *p4 = heap_malloc(104);
-
-    CU_ASSERT(p4 == NULL);
-}
-
-void test_heap_free_several()
-{
-    init_heap();
-
-    char *p1 = heap_malloc(10);
-    char *p2 = heap_malloc(10);
-    char *p3 = heap_malloc(10);
-    char *p4 = heap_malloc(10);
-
-    strcpy(p1, "tp1");
-    strcpy(p2, "tp2");
-    strcpy(p3, "tp3");
-    strcpy(p4, "tp4");
-
-    heap_free(p2); // simple free
-
-    CU_ASSERT(*(p2 - 1) == 10);
-    CU_ASSERT(*(p2) == FREE_ZONE);
-    CU_ASSERT(libre == p2 - 1 - heap);
-
-    heap_free(p3); // testing merge left
-
-    CU_ASSERT(*(p3) == FREE_BLOCK);
-    CU_ASSERT(*(p3 - 1) == FREE_BLOCK);
-
-    CU_ASSERT(*(p2 - 1) == 21);
-    CU_ASSERT(*(p2) == FREE_ZONE);
-    CU_ASSERT(libre == p2 - 1 - heap);
-
-    heap_free(p1); // testing merge right
-
-    CU_ASSERT(*(p1 - 1) == 32);
-    CU_ASSERT(*(p1) == FREE_ZONE);
-    CU_ASSERT(libre == 0);
-}
-
-void test_full_example()
-{
-    init_heap();
-
-    char *p1 = heap_malloc(10);
-    char *p2 = heap_malloc(9);
-    char *p3 = heap_malloc(5);
-
-    strcpy(p1, "tp 1");
-    strcpy(p2, "tp 2");
-    strcpy(p3, "tp 3");
-
-    heap_free(p2);
-
-    // That verifies that in case we have 1 byte left we actually malloc size+1
-    char *p4 = heap_malloc(8);
-    strcpy(p4, "systeme");
-
-    // display_heap(127);
-
-    CU_ASSERT(*(p1 - 1) == 10);
-    CU_ASSERT(*(p4 - 1) == 9);
-    CU_ASSERT(*(p3 - 1) == 5);
-    CU_ASSERT(libre == 27);
-    CU_ASSERT(heap[27] == 100);
-    CU_ASSERT(heap[28] == FREE_ZONE);
-
-    CU_ASSERT(strcmp(p1, "tp 1") == 0);
-    CU_ASSERT(strcmp(p3, "tp 3") == 0);
-    CU_ASSERT(strcmp(p4, "systeme") == 0);
+    CU_ASSERT((int)libre.start->data == 4);
 }
 
 int init_suite(void) { return 0; }
@@ -247,13 +158,9 @@ int main()
         NULL == CU_add_test(pSuite, "test of test_init_heap()", test_init_heap) ||
         NULL == CU_add_test(pSuite, "test of test_first_fit()", test_first_fit) ||
         NULL == CU_add_test(pSuite, "test of test_add_character()", test_add_character) ||
-        NULL == CU_add_test(pSuite, "test of test_empty_heap()", test_empty_heap) ||
         NULL == CU_add_test(pSuite, "test of test_add_to_empty_heap()", test_add_to_empty_heap) ||
         NULL == CU_add_test(pSuite, "test of test_add_to_almost_empty_heap()", test_add_to_almost_empty_heap) ||
-        NULL == CU_add_test(pSuite, "test of test_heap_free()", test_heap_free) ||
-        NULL == CU_add_test(pSuite, "test of test_heap_malloc_example()", test_heap_malloc_example) ||
-        NULL == CU_add_test(pSuite, "test of test_heap_free_several()", test_heap_free_several) ||
-        NULL == CU_add_test(pSuite, "test of test_full_example()", test_full_example)
+        NULL == CU_add_test(pSuite, "test of test_heap_free()", test_heap_free)
 
     )
     {
