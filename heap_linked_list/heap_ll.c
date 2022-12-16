193f488fe922275781ff3207f36175a7770c81ba
nolwenn david <nolwenn-david@hotmail.fr>
2022-12-16 10:12:14 +0100

    libre with linked_list

--- heap_linked_list/heap_ll.c
+++ heap_linked_list/heap_ll.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "list_functions.h"
+#include "list.h"
 #include "heap_ll.h"
 
-void init_heap()
+void init_heap(list_t *libre)
 {
     heap[0] = SIZE_HEAP - 1;
     heap[1] = FREE_ZONE;
@@ -13,13 +13,20 @@ void init_heap()
     {
         heap[i] = FREE_BLOCK;
     }
-    libre = 0;
+
+    // uint8_t first_free_index = 0;
+    while (libre->start != NULL)
+    {
+        list_pop_first(libre);
+    }
+
+    list_append(libre, (void *)0);
 }
 
-void display_heap(int size)
+void display_heap(int size, list_t *libre)
 {
-    int i = 0;
-    int count = heap[0];
+    uint8_t i = 0;
+    uint8_t count = heap[0];
 
     printf("\n### heap ###\n");
     printf("%d => %d\n", 0, count);
@@ -52,79 +59,116 @@ void display_heap(int size)
         }
     }
 
-    printf("Libre = %d\n", libre);
+    element_t *elem = libre->start;
+    printf("Libre : \n");
+    while (elem != NULL)
+    {
+        printf("%d\n", (int)elem->data);
+        elem = elem->next;
+    }
 }
 
-int get_index_next_block(int index)
-{
-    return heap[index] + index + 1;
-}
+// int get_index_next_block(int index)
+// {
+//     return heap[index] + index + 1;
+// }
 
-int first_fit(int size)
+int8_t first_fit(uint8_t size, list_t *libre)
 {
-    if (libre == MEMORY_FULL)
+    if (libre->start == NULL)
         return MEMORY_FULL;
 
-    // sinon trouver -1 et vérifier qu'on a la place
-    int index = libre;
+    // sinon parcourir les espaces libres et voir si on a assez de place pour allouer
+    element_t *element;
+    element = libre->start;
 
-    while (index < SIZE_HEAP)
+    while (element != NULL)
     {
-        if (heap[index + 1] == FREE_ZONE && heap[index] >= size)
-            return index;
+        int8_t index = (int)element->data;
+        if (heap[index] >= size)
+            return (int)element->data;
 
-        index = get_index_next_block(index);
+        element = element->next;
     }
+
     return MEMORY_FULL;
 }
 
-int worst_fit(int size)
+// int worst_fit(int size)
+// {
+//     int max = 0;
+//     int max_index = MEMORY_FULL;
+
+//     int index = libre;
+
+//     while (index < SIZE_HEAP) // Sauter de block en block
+//     {
+//         if (heap[index + 1] == FREE_ZONE && heap[index] >= size && heap[index] > max)
+//         {
+//             max = heap[index];
+//             max_index = index;
+//         }
+//         index = get_index_next_block(index);
+//     }
+//     return max_index;
+// }
+
+// int best_fit(int size)
+// {
+//     int nearest = SIZE_HEAP;
+//     int near_index = MEMORY_FULL;
+
+//     int index = libre;
+
+//     while (index < SIZE_HEAP)
+//     {
+//         if (heap[index + 1] == FREE_ZONE && heap[index] >= size && heap[index] - size < nearest)
+//         {
+//             nearest = heap[index] - size;
+//             near_index = index;
+//         }
+//         index = get_index_next_block(index);
+//     }
+//     return near_index;
+// }
+
+// void update_libre(int free_index)
+// {
+//     // free_index vient d'être allouer
+//     Liste *tmp, *tmp_prev;
+//     int index_free;
+//     index_free = first_fit(1);
+//     printf("index_free %d\n", index_free);
+//     tmp = libre;
+//     if (libre->number >= index_free)
+//     {
+//         tmp->number = index_free;
+//         tmp->next = libre;
+//         libre = tmp;
+//         return;
+//     }
+
+//     tmp_prev = libre;
+//     tmp = tmp->next;
+//     while (tmp->number < index_free || tmp->next != NULL)
+//     {
+//         tmp = tmp->next;
+//         tmp_prev = tmp_prev->next;
+//     }
+//     tmp->number = index_free;
+//     tmp_prev->next = tmp;
+// }
+
+char *heap_malloc(uint8_t size, list_t *libre, int8_t (*strategie)(uint8_t, list_t *))
 {
-    int max = 0;
-    int max_index = MEMORY_FULL;
 
-    int index = libre;
+    int8_t free_index = strategie(size, libre);
 
-    while (index < SIZE_HEAP) // Sauter de block en block
-    {
-        if (heap[index + 1] == FREE_ZONE && heap[index] >= size && heap[index] > max)
-        {
-            max = heap[index];
-            max_index = index;
-        }
-        index = get_index_next_block(index);
-    }
-    return max_index;
-}
-
-int best_fit(int size)
-{
-    int nearest = SIZE_HEAP;
-    int near_index = MEMORY_FULL;
-
-    int index = libre;
-
-    while (index < SIZE_HEAP)
-    {
-        if (heap[index + 1] == FREE_ZONE && heap[index] >= size && heap[index] - size < nearest)
-        {
-            nearest = heap[index] - size;
-            near_index = index;
-        }
-        index = get_index_next_block(index);
-    }
-    return near_index;
-}
-
-char *heap_malloc(unsigned int size, int (*strategie)(int))
-{
-
-    int free_index = strategie(size);
-
+    printf("return strategy : %d", free_index);
     if (free_index == MEMORY_FULL)
         return NULL;
 
-    int size_free_zone = heap[free_index];
+    uint8_t size_free_zone = heap[free_index];
 
     // Allocation zone prise
     if (size_free_zone == size + 1) // S'il y a une miette derrière
@@ -133,20 +177,26 @@ char *heap_malloc(unsigned int size, int (*strategie)(int))
     heap[free_index] = size;
     heap[free_index + 1] = FREE_BLOCK;
 
+    int32_t index_rm = list_index(libre, (void *)free_index);
+    list_remove_at(libre, index_rm);
+
     // Définition zone libre derrière (si besoin)
     if (size_free_zone >= size + 2 && free_index + size + 2 < SIZE_HEAP)
     {
         heap[free_index + size + 1] = size_free_zone - size - 1;
         heap[free_index + size + 2] = FREE_ZONE;
+
+        // Ajouter un élément à libre
+        int index_new_free = free_index + size + 1;
+        list_append(libre, (void *)index_new_free); // a trier
     }
 
-    libre = first_fit(1);
     return &heap[free_index + 1];
 }
 
-void heap_free(char *ptr)
+void heap_free(char *ptr, list_t *libre)
 {
-    int index_ptr;
+    int8_t index_ptr;
 
     *ptr = FREE_ZONE;
     for (int i = 1; i < *(ptr - 1); ++i)
@@ -154,42 +204,43 @@ void heap_free(char *ptr)
         *(ptr + i) = FREE_BLOCK;
     }
 
-    index_ptr = ptr - heap;
+    index_ptr = ptr - heap - 1;
 
-    if (libre > index_ptr)
-        libre = index_ptr - 1;
+    // ajouter l'index dans free // A trier
 
-    int bool = 1;
+    list_append(libre, (void *)index_ptr);
 
-    while (bool)
-    {
-        bool = search_two_free_zone();
-    }
+    // int bool = 1;
+
+    // while (bool)
+    // {
+    //     bool = search_two_free_zone();
+    // }
 }
 
-void add_two_zone_free(int first_zone)
-{
-    int len_first_zone = heap[first_zone];
-    int index_second_zone = len_first_zone + first_zone + 1;
+// void add_two_zone_free(int first_zone)
+// {
+//     int len_first_zone = heap[first_zone];
+//     int index_second_zone = len_first_zone + first_zone + 1;
 
-    heap[first_zone] = len_first_zone + heap[index_second_zone] + 1;
-    heap[index_second_zone] = FREE_BLOCK;
-    heap[index_second_zone + 1] = FREE_BLOCK;
-}
+//     heap[first_zone] = len_first_zone + heap[index_second_zone] + 1;
+//     heap[index_second_zone] = FREE_BLOCK;
+//     heap[index_second_zone + 1] = FREE_BLOCK;
+// }
 
-int search_two_free_zone()
-{
-    int index = libre;
+// int search_two_free_zone()
+// {
+//     int index = libre;
 
-    while (index < SIZE_HEAP)
-    {
-        if (heap[index + 1] == FREE_ZONE && heap[heap[index] + index + 2] == FREE_ZONE)
-        {
-            add_two_zone_free(index);
-            return 1;
-        }
-        index = get_index_next_block(index);
-    }
+//     while (index < SIZE_HEAP)
+//     {
+//         if (heap[index + 1] == FREE_ZONE && heap[heap[index] + index + 2] == FREE_ZONE)
+//         {
+//             add_two_zone_free(index);
+//             return 1;
+//         }
+//         index = get_index_next_block(index);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
--- heap_linked_list/heap_ll.h
+++ heap_linked_list/heap_ll.h
@@ -1,7 +1,7 @@
 #ifndef HEAP_LL_HEADER
 #define HEAP_LL_HEADER
 
-#include "list_functions.h"
+#include "list.h"
 
 #define SIZE_HEAP 128
 #define FREE_BLOCK '\0'
@@ -10,19 +10,18 @@
 #define MEMORY_FULL -1
 
 char heap[SIZE_HEAP];
-int libre;
 
-void init_heap();
-void display_heap(int size);
+void init_heap(list_t *libre);
+void display_heap(int size, list_t *libre);
 
 // Stratégies
-int (*strategie)(int);
-int worst_fit(int size);
-int first_fit(int size);
-int best_fit(int size);
-char *heap_malloc(unsigned int taille, int (*strategie)(int));
+int8_t (*strategie)(uint8_t, list_t *);
+int8_t worst_fit(uint8_t size);
+int8_t first_fit(uint8_t size, list_t *libre);
+int8_t best_fit(uint8_t size);
+char *heap_malloc(uint8_t size, list_t *libre, int8_t (*strategie)(uint8_t, list_t *));
 
-void heap_free(char *ptr);
+void heap_free(char *ptr, list_t *libre);
 
 // Deux zones libres à la suite
 int search_two_free_zone();
