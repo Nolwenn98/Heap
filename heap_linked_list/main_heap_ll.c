be3268175c615deba252ae2a5aa30b14e0ac1000
nolwenn david <nolwenn-david@hotmail.fr>
2022-12-16 13:40:24 +0100

    update main

--- heap_linked_list/main_heap_ll.c
+++ heap_linked_list/main_heap_ll.c
@@ -36,31 +36,5 @@ int main()
     printf("p4 : %d", *p4);
     strcpy(p4, "systeme");
 
-    // char *p1, *p2, *p3, *p4;
-
-    // p1 = (char *)heap_malloc(10, strategie);
     display_heap(30, &libre);
-    // p2 = (char *)heap_malloc(9, strategie);
-    // p3 = (char *)heap_malloc(5, strategie);
-
-    // strcpy(p1, "tp 1");
-    // strcpy(p2, "tp 2");
-    // strcpy(p3, "tp 3");
-    // display_heap(30);
-
-    // heap_free(p2);
-    // p4 = (char *)heap_malloc(5, strategie);
-    // strcpy(p4, "abc");
-    // display_heap(30);
-
-    // heap_free(p1);
-    // p4 = (char *)heap_malloc(5, strategie);
-    // strcpy(p4, "def");
-    // display_heap(30);
-
-    // p4 = (char *)heap_malloc(3, strategie);
-    // strcpy(p4, "ab");
-    // display_heap(30);
-
-    // return 0;
 }
